09af538afb63f568aa4f7f56c1ff4ff16954a07b
pawelkozolub <pkozolub@gmail.com>
2024-02-03 23:29:00 +0100

    Extract symbol from name

--- src/main/java/org/example/StockScraper.java
+++ src/main/java/org/example/StockScraper.java
@@ -14,18 +14,18 @@ public class StockScraper {
 
         try {
             Document doc = Jsoup.connect(URL).get();
-
             Elements items = doc.select(".qTableFull tr");
 
             int i = 0;
             for (Element item : items) {
                 if (!skipElement(item)) {
                     i++;
+                    String symbol = item.select("a").text().substring(0, 3);
                     String name = item.select("a").text();
                     String time = item.select(".q_ch_date").attr("datetime");
                     String close = item.select(".q_ch_act").text();
                     String closePct = item.select(".q_ch_per").text();
-                    System.out.printf("%d %s %s %s %s\n", i, name, time, close, closePct);
+                    System.out.printf("%d %s %s %s %s %s\n", i, symbol, name, time, close, closePct);
                 }
             }
 
